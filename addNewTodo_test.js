3c7e8c1bdf3129c3623c54c3433a5ba155b3c066
Michał <kontakt@testingplus.me>
2017-03-02 21:28:03 +0100

    fix first test in codeceptJS

--- addNewTodo_test.js
+++ addNewTodo_test.js
@@ -1,8 +1,8 @@
 
 Feature('Adding a new todo to app');
-let homePage = require('./pages/homePage.js');
+
 Scenario('Add item to list', (I,homePage) => {
     I.amOnPage('/');
-    homePage.newTodo(text);
+    homePage.newTodo('ss');
     
 });
