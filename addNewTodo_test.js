8ca1a38ce777e3c4a4ce15b1a4afea6da0405c45
Michał <kontakt@testingplus.me>
2017-03-12 17:58:55 +0100

    Take count value and check correctness

--- addNewTodo_test.js
+++ addNewTodo_test.js
@@ -1,8 +1,12 @@
-
+var assert=require('assert');
 Feature('Adding a new todo to app');
-
-Scenario('Add item to list', (I,homePage) => {
-    I.amOnPage('/');
+Before((I) =>{
+      I.amOnPage('/');
+});
+Scenario('Add item to list', function*(I,homePage){
     homePage.newTodo('ss');
-    
+    var count = yield I.grabTextFrom('#todo-count > strong');
+   I.say(count);
+   assert.equal(count,'1','Bad counted function');
+   I.say('Good counted');
 });
