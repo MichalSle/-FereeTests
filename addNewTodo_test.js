dfb0ad434cfd0d18cf6ee305366981d2a8bdc2ec
Michał <kontakt@testingplus.me>
2017-03-18 21:41:18 +0100

    fix

--- addNewTodo_test.js
+++ addNewTodo_test.js
@@ -10,8 +10,6 @@ Scenario('Click to more button on homePage', function*(I,homePage){
    // var countBeforeClick= element.all(by.css('.right'));
     var textFromFirst=yield I.grabTextFrom('span');
     expect(textFromFirst).to.equal('test user 1');
-   // var a= this.helpers['commonHelper'].findElements('span');
-    //expect(a.length).to.equal(3);
     var a=  I.findElements('span');
     I.say(a);
     I.click(homePage.moreButton);
